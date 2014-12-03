824112d5385b8b35db6136da17a6e762bd580b61
Zerocool <Zerocool@fakemail.com>
2014-12-03 12:51:58 +0100

    ignore commit problems

--- src/FindLinks.java
+++ src/FindLinks.java
@@ -5,29 +5,55 @@ import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 
 import tornado.org.cypherspider.AlternateCrawler;
+import tornado.org.neo4j.Database;
+
+import tornado.org.cypherspider.AlternateCrawler;
 import tornado.org.neo4j.ProductDatabase;
 
-public class FindLinks {
-    private static ArrayList<String> links = new ArrayList<>();
-    private static ArrayList<String> productlinks = new ArrayList<>();
-    private static ArrayList<String> productnr = new ArrayList<>();
+public class FindLinks extends Thread {
 
-    private static String productListingUrlModifier = "&size=500#listingResult";
-    private static String url = "http://www.alternate.nl/html/highlights/page.html?hgid=205&tgid=944&tk=7&lk=9276";
+	private final CharSequence paternListinLink = "/html/product/listing";
+	private final String urlAlternate = "http://www.alternate.nl";
+	private final String paternProductLink = "/html/product/";
+	private final CharSequence paternNavigationLink = "/html/highlights/page";
+	private final String productListingUrlModifier = "&size=500#listingResult";
+
+	private ArrayList<String> links = new ArrayList<>();
+	private ArrayList<String> productlinks = new ArrayList<>();
+	private ArrayList<String> productnr = new ArrayList<>();
+
+	private static org.jsoup.nodes.Document doc;
+	private Elements e;
+	private final ProductDatabase productDatabase = new ProductDatabase();
+
+	private final AlternateCrawler alternateCrawler = new AlternateCrawler();
+
+	private static String url = "http://www.alternate.nl/html/highlights/page.html?hgid=205&tgid=944&tk=7&lk=9276";
 
 	private static int sizeProductNr = 7;
 
-	public final static void main(String[] args) throws IOException {
+	/*
+	 * Maakt contact met alternate en begint vervolgens de graph database te
+	 * vullen in een aparte thread
+	 * 
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Thread#run()
+	 */
+	public void run() {
 
-		org.jsoup.nodes.Document doc = Jsoup.connect(url).get();
-		Elements e = doc.getElementById("navTree").getElementsByTag("a");
-		// System.out.print(e.toString());
+		try {
+			doc = Jsoup.connect(url).get();
+			e = doc.getElementById("navTree").getElementsByTag("a");
+			parselinks(e);
 
-		parselinks(e);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
 		for (int i = 0; i < links.size(); i++) {
-			url = "http://www.alternate.nl" + links.get(i);
-			// System.out.println("\n-------   " + url);
+			url = urlAlternate + links.get(i);
 
 			try {
 				doc = Jsoup.connect(url).get();
@@ -35,17 +61,47 @@ public class FindLinks {
 				parselinks(e);
 
 			} catch (Exception exp) {
+				exp.printStackTrace();
 			}
 
 		}
 
-		System.out.print(links.toString());
+		findProducts();
+		insertProducts();
 
+	}
+
+	private void insertProducts() {
+		productDatabase.createDB();
+
+		for (int i = 0; i < productlinks.size(); i++) {
+
+			try {
+				String producturl = productlinks.get(i);
+				int startpoint = producturl.indexOf(paternProductLink) + 14;
+				String nr = producturl.substring(startpoint, startpoint
+						+ sizeProductNr);
+				nr = nr.replace("?", "");
+				nr = nr.replace("t", "");
+
+				productnr.add(nr);
+				// TODO controlleer wrm hij naa een lange periode een
+				// java.systeem.outofmemory error geeft
+				alternateCrawler.crawl(nr, productDatabase);
+
+			} catch (Exception exp) {
+				exp.printStackTrace();
+			}
+		}
+
+	}
+
+	private void findProducts() {
 		for (int i = 0; i < links.size(); i++) {
 
 			String listinglink = links.get(i);
 
-			if (listinglink.contains("/html/product/listing")) {
+			if (listinglink.contains(paternListinLink)) {
 
 				listinglink = listinglink + productListingUrlModifier;
 				url = "http://www.alternate.nl" + listinglink;
@@ -57,7 +113,6 @@ public class FindLinks {
 					for (int j = 0; j < e.size(); j++) {
 						String plink = e.get(j).getElementsByTag("a")
 								.attr("href");
-						// System.out.println(plink);
 						productlinks.add(plink);
 
 					}
@@ -69,47 +124,17 @@ public class FindLinks {
 			}
 		}
 
-		final ProductDatabase db = new ProductDatabase();
-		db.createDB();
-
-		final AlternateCrawler alternateCrawler = new AlternateCrawler();
-
-		for (int i = 0; i < productlinks.size(); i++) {
-
-			try {
-				String producturl = productlinks.get(i);
-				// System.out.println(producturl);
-				int startpoint = producturl.indexOf("/html/product/") + 14;
-				String nr = producturl.substring(startpoint, startpoint
-						+ sizeProductNr);
-				nr = nr.replace("?", "");
-				nr = nr.replace("t", "");
-
-				productnr.add(nr);
-				// System.out.println(nr);
-
-                alternateCrawler.crawl(nr, db);
-
-			} catch (Exception exp) {
-				exp.printStackTrace();
-			}
-		}
-
 	}
 
-	private static void parselinks(Elements elements) {
+	private void parselinks(Elements elements) {
 		for (int i = 0; i < elements.size(); i++) {
 
 			String link = elements.get(i).attr("href");
 
-			// System.out.println(i + "  " + elements.get(i).attr("href") +
-			// "\n");
-
-			if (link.contains("/html/highlights/page")
-					|| link.contains("/html/product/")) {
+			if (link.contains(paternNavigationLink)
+					|| link.contains(paternProductLink)) {
 
 				if (!links.contains(link)) {
-					// System.out.println("++++++adding link " + links.size());
 					links.add(link);
 				}
 			}
