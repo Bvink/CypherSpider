e298ed00f5d1af4c003290ad75927621f8c5d9c9
Brandon Vink <vinkbrandon@gmail.com>
2014-12-17 14:59:03 +0100

    CypherSpider                          - Added endstate Settings

--- src/tornado/org/Start.java
+++ src/tornado/org/Start.java
@@ -2,12 +2,16 @@ package tornado.org;
 
 import javafx.application.Application;
 import tornado.org.fx.Gui;
+import tornado.org.settings.Settings;
 
 
 public class Start {
 
     public static void main(String[] args) throws Exception {
 
+        Settings.setAlternateEndstate(false);
+        Settings.setMycomEndstate(false);
+        Settings.setParadigitEndstate(false);
         /**
     	FindLinksOnAlternate findLinks = new FindLinksOnAlternate();
     	findLinks.start();
