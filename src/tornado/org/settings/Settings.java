87c37a4109d249155688c99229f7a853bca252fe
Brandon Vink <vinkbrandon@gmail.com>
2014-12-17 14:55:10 +0100

    CypherSpider - Added endstate Settings

---
+++ src/tornado/org/settings/Settings.java
@@ -0,0 +1,33 @@
+package tornado.org.settings;
+
+public class Settings {
+
+    private static boolean alternateEndstate = false;
+    private static boolean mycomEndstate = false;
+    private static boolean paradigitEndstate = false;
+
+    public static void setAlternateEndstate(boolean state) {
+        alternateEndstate = state;
+    }
+
+    public static void setMycomEndstate(boolean state) {
+        mycomEndstate = state;
+    }
+
+    public static void setParadigitEndstate(boolean state) {
+        paradigitEndstate = state;
+    }
+
+    public static boolean getAlternateEndstate() {
+        return alternateEndstate;
+    }
+
+    public static boolean getMycomEndstate() {
+        return mycomEndstate;
+    }
+
+    public static boolean getParadigitEndstate() {
+        return paradigitEndstate;
+    }
+
+}
