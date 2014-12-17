9ff63d896b3bd057eb15f5e4d2bc7dd24e18fb04
Brandon Vink <vinkbrandon@gmail.com>
2014-12-17 16:02:58 +0100

    CypherSpider - Added a start switch

--- src/tornado/org/Start.java
+++ src/tornado/org/Start.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import tornado.org.fx.Gui;
 import tornado.org.settings.Settings;
 
+import java.util.List;
+
 
 public class Start {
 
@@ -12,15 +14,48 @@ public class Start {
         Settings.setAlternateEndstate(false);
         Settings.setMycomEndstate(false);
         Settings.setParadigitEndstate(false);
-        /**
-    	FindLinksOnAlternate findLinks = new FindLinksOnAlternate();
-    	findLinks.start();
-    	FindLinksOnMycom mycom = new FindLinksOnMycom() ;
-    	mycom.start();
-    	FindLinksOnParadigit onParadigit = new FindLinksOnParadigit() ; 
-    	onParadigit.start();
-    	**/
 
-        Application.launch(Gui.class, (String[]) null);
+        switch (args[0].toLowerCase()) {
+            case "crawl":
+                startAlternateCrawler();
+                startMycomCrawler();
+                startParadigitCrawler();
+                break;
+            case "alternate":
+                startAlternateCrawler();
+                break;
+            case "mycom":
+                startMycomCrawler();
+                break;
+            case "paradigit":
+                startParadigitCrawler();
+                break;
+            case "debug":
+                Application.launch(Gui.class, (String[]) null);
+                break;
+            default:
+                System.out.println("Please supply an argument!");
+                System.out.println("crawl");
+                System.out.println("alternate");
+                System.out.println("mycom");
+                System.out.println("paradigit");
+                System.out.println("debug");
+                break;
+        }
+    }
+
+    private static void startAlternateCrawler() {
+        FindLinksOnAlternate alternate = new FindLinksOnAlternate();
+        alternate.start();
+    }
+
+    private static void startMycomCrawler() {
+        FindLinksOnMycom mycom = new FindLinksOnMycom();
+        mycom.start();
+    }
+
+    private static void startParadigitCrawler() {
+        FindLinksOnParadigit paradigit = new FindLinksOnParadigit();
+        paradigit.start();
     }
 }
